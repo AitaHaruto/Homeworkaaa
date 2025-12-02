ace0f0e1413577124899860edd611f0666a037ca
AitaHaruto <mazirenja0212@gmail.com>
2025-12-02 23:51:43 +0900

    連続計算対応

--- kimatu kadai/kimatu kadai/Form1.cs
+++ kimatu kadai/kimatu kadai/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,56 +13,93 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-       
+
         private string enzanshi;
         private double num1;
         private double num2;
-         
+
 
 
         public Form1()
         {
             InitializeComponent();
-        }  
-    
-       
+        }
+
+        private void RenzokuKeisan()
+        {
+             textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+                     num1 = Convert.ToDouble(textBox1.Text);
+                     num2=  Convert.ToDouble(textBox2.Text);
+                      
+                switch (enzanshi)
+                {
+                    case "+": textBox2.Text = Convert.ToString(num1 + num2); break;
+                    case "-": textBox2.Text = Convert.ToString(num1 - num2); break;
+                    case "×": textBox2.Text = Convert.ToString(num1 * num2); break;
+                    case "÷":
+                        if (num2 == 0)
+                        {
+                            textBox1.Text = "Error";
+                        }
+                        else
+                        {
+                            textBox2.Text = Convert.ToString(num1 / num2);
+                        }
+                        break;
+
+                }
+                textBox1.Text = "";
+                enzanshi = null;
+            
+        }
+
+
 
         private void NumberButton_Click(object sender, EventArgs e)
         {
-           Button btn = (Button)sender;
+            Button btn = (Button)sender;
             textBox1.Text += btn.Text;
         }
-      
+
 
         private void Clear_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
             textBox2.Text = "";
         }
-        private void Operator(object sender,EventArgs e)
+        private void Operator(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            if(textBox1.Text!="")
+            if (textBox1.Text != "")
             {
-                    num1=Convert.ToDouble(textBox1.Text);
-                    textBox2.Text = textBox1.Text + btn.Text;
-                    textBox1.Text = "";
-                    enzanshi = btn.Text;
+                if(textBox2.Text!="")
+                {
+                    RenzokuKeisan();
+                }
+                else
+                {
+                     num1 = Convert.ToDouble(textBox1.Text);
+                textBox2.Text = textBox1.Text + btn.Text;
+                textBox1.Text = "";
+                enzanshi = btn.Text;
 
+                }
+
+               
             }
         }
 
-       
+
         private void dott_Click(object sender, EventArgs e)
         {  //小数点を複数押せないようにする.
-           
+
             if (!textBox1.Text.Contains("."))
             {
-               if(textBox1.Text=="")
+                if (textBox1.Text == "")
                 {
-                     textBox1.Text="0.";  
+                    textBox1.Text = "0.";
 
-                } 
+                }
                 else
                 {
                     textBox1.Text = textBox1.Text + ".";
@@ -83,26 +121,26 @@ namespace WindowsFormsApp1
 
             if (textBox1.Text != "")
             {
-              num2 = Convert.ToDouble(textBox1.Text);
-            switch(enzanshi)
+                num2 = Convert.ToDouble(textBox1.Text);
+                switch (enzanshi)
                 {
-                    case "+":textBox1.Text=Convert.ToString(num1+num2);break;
-                    case "-":textBox1.Text=Convert.ToString(num1-num2);break;
-                    case "×":textBox1.Text=Convert.ToString(num1*num2);break;
+                    case "+": textBox1.Text = Convert.ToString(num1 + num2); break;
+                    case "-": textBox1.Text = Convert.ToString(num1 - num2); break;
+                    case "×": textBox1.Text = Convert.ToString(num1 * num2); break;
                     case "÷":
-                    if(num2==0)
+                        if (num2 == 0)
                         {
-                            textBox1.Text="Error";
+                            textBox1.Text = "Error";
                         }
                         else
                         {
-                             textBox1.Text=Convert.ToString(num1/num2);
+                            textBox1.Text = Convert.ToString(num1 / num2);
                         }
                         break;
-                   
+
                 }
-            textBox2.Text = "";
-            enzanshi = null;
+                textBox2.Text = "";
+                enzanshi = null;
             }
 
         }
@@ -113,25 +151,25 @@ namespace WindowsFormsApp1
         }
         private void Subetteru()
         {
-            if(textBox1.Text=="4545"||textBox1.Text=="1919")
-             {
+            if (textBox1.Text == "4545" || textBox1.Text == "1919")
+            {
                 MessageBox.Show("君、すべってるよ。");
-                textBox1.Text="";
+                textBox1.Text = "";
                 Application.Exit();
-             } 
+            }
         }
         private void TextBox1_TextChanged(object sender, EventArgs e)
-        { 
-            
+        {
+
             Subetteru();
-            
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
-        
-       
+
+
     }
 }
